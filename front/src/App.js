a695ed769df3ecc83ab954b06b6cb96b937ac921
minjin <shinhwa03_24@naver.com>
2023-08-02 20:11:10 +0900

    내 브랜치에 프론트 소스 커밋_여민진

--- front/src/App.js
+++ front/src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React 테스트_민주
+          Learn React 테스트_민진
         </a>
       </header>
     </div>
