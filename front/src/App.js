ca1766bdd5768616fd556a9983730cc68d9b4bf0
miinjukang <smile_web@naver.com>
2023-08-02 19:39:34 +0900

    vs 커밋 테스트

--- front/src/App.js
+++ front/src/App.js
@@ -14,8 +14,8 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React 테스트커
+        >  
+          Learn React 테스트_민주
         </a>
       </header>
     </div>
