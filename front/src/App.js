d2ad38e6194fecef8c3b35ca80d6a703f35e1a1b
kimdahhh <dahuin4566@gmail.com>
2023-08-19 16:04:43 +0900

    다흰 > test

--- front/src/App.js
+++ front/src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React 테스트
+          Learn React 테스트_2
         </a>
       </header>
     </div>
